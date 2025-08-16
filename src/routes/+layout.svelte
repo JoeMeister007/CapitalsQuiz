64890c1ebb49b251094add97e469313b9e6c3ff8
jdlangho <joemeister007@gmail.com>
2025-08-16 09:45:52 -0400

    Change bootstrap inclusion method to not reference node_modules

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 
+	import 'bootstrap/dist/css/bootstrap.min.css';
+    import { onMount } from 'svelte';
+	// Import Bootstrap JS
+	// We do this in onMount so it's only loaded in the browser
+	// prevents SSR errors
+    onMount(async () => {
+        await import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    });
 	let { children } = $props();
 </script>
 
